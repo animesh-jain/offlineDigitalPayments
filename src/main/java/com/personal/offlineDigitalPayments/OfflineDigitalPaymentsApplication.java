d6825e0c7fb6f8c4c65f6e2a02a78f30e7a53776
animeshjain <animesh.jain@housing.com>
2023-10-29 17:00:36 +0530

    New Project Setup

---
+++ src/main/java/com/personal/offlineDigitalPayments/OfflineDigitalPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.personal.offlineDigitalPayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfflineDigitalPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfflineDigitalPaymentsApplication.class, args);
+	}
+
+}
